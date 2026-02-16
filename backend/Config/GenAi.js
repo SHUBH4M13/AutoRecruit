d41ad5d3159973127330906b0694d684ac431106
Shubham Karna <skarna230@gmail.com>
2026-02-16 23:00:55 +0530

    Added genai config

---
+++ backend/Config/GenAi.js
@@ -0,0 +1,11 @@
+import openai from "openai"
+import dotenv from dotenv
+
+dotenv.config();
+
+const client = new openai({
+    apiKey: process.env.GROQ_API_KEY,
+    baseURL: "https://api.groq.com/openai/v1",
+})
+
+export default client
